7f004767360475041399a09428774aa3114479ad
Avenger2627 <imraninfo97@gmail.com>
2020-11-29 17:39:20 +0530

    [Imran Shaikh] Refactor: The Tic Tac Toe Method Added

--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -8,31 +8,36 @@ namespace BasicProgram
 			int ch;
 			while (true)
 			{
-				Console.Write("\n1.Hello World \n 2.Power of Number \n 3.Leap Year \n 4.Addition \n 5.GuessNumber \n Enter Your Choice :   ");
+				Console.Write("\n1.Hello World \n 2.Power of Number \n 3.Leap Year \n 4.Addition \n 5.GuessNumber" +
+				"\n6.Tic \n Enter Your Choice :   ");
 				ch = Convert.ToInt32(Console.ReadLine());
 				switch (ch)
 				{
 					case 1:
 						Hello.HelloWorld();
-						break;
+					break;
 					case 2:
 						PowerofNumber.Power();
-						break;
+					break;
 					case 3:
 						LeapYear.LepYear();
-						break;
+					break;
 					case 4:
 						Addition.Adition();
-						break;
+					break;
 					case 5:
 						GuessNumber.GuesNumber();
-						break;
-
+					break;
+					case 6:
+						TicDry.Tic();
+					break;
+					case 7:
+						MergeSort.Merge();
 					default:
-						Console.WriteLine("\nEnvalid Choice....");
+						Console.WriteLine("\n Invalid Choice....");
 					break;
 				}
 			}
-		}			
+		}
 	}//class
 }//namespace
